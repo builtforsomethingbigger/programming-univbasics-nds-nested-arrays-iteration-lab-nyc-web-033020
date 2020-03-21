32ca22544a6ab8145d12d48da5931f9515716988
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-21 19:46:49 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -11,8 +11,8 @@ def join_ingredients(src)
   while row_index < src.count do
   	outer_results << src[row_index]
   	row_index += 1
+    puts "I love #{outer_results[row_index[0]]} and #{outer_results[row_index[1]]} on my pizza"
   end
-  puts "I love #{outer_results[row_index[0]]} and #{outer_results[row_index[1]]} on my pizza"
 end
 
 def find_greater_pair(src)
